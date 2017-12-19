b61ebf09932b48c25195382e90e049a8433700a5
Eugene <eresid@gmail.com>
2017-12-19 12:08:39 +0200

    Init commit

---
+++ src/main/java/com/github/eresid/blog/SimpleBlog/SimpleBlogApplication.java
@@ -0,0 +1,12 @@
+package com.github.eresid.blog.SimpleBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogApplication.class, args);
+	}
+}
